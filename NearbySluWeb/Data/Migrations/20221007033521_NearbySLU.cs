0f56cd6406fb1e82fc835b4fc609217e06dc8926
AbhilashKotha <113061137+AbhilashKotha@users.noreply.github.com>
2022-10-07 03:06:33 -0500

    TC-24 Initial skeleton commit with login and register functionality
    TC-24 Initial skeleton commit with login and register functionality

---
+++ NearbySluWeb/Data/Migrations/20221007033521_NearbySLU.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NearbySluWeb.Data.Migrations
+{
+    public partial class NearbySLU : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
