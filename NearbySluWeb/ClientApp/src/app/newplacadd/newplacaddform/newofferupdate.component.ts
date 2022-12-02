6cf3b4a30e741e0cd859e6ccb84b3cc6da3aa989
manit <maniteja.akinapalli@slu.edu>
2022-12-02 13:34:45 -0600

    added comments to the commmits

--- NearbySluWeb/ClientApp/src/app/newplacadd/newplacaddform/newofferupdate.component.ts
+++ NearbySluWeb/ClientApp/src/app/newplacadd/newplacaddform/newofferupdate.component.ts
@@ -2,7 +2,7 @@ updateRecord(form: NgForm) {
   this.service.putNewofferData().subscribe(
     res => {
       this.resetForm(form);
-      this.service.refreshList();
+      this.service.refreshList();// refreshing the form of the offer function.
       setTimeout(() => { this.isNewofferPosted = false; }, 1000);
     },
     err => { console.log(err); }
